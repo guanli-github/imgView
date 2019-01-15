8dc34c165b8a44687b270bdd4ebdf8146c1e924e
guanli <guan__li@yeah.net>
2019-01-15 11:27:09 +0800

    add bookmark function

---
+++ src/main/java/data/BookMark.java
@@ -0,0 +1,59 @@
+package data;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
+
+public class BookMark {
+    private static String savePath = "D:/1.txt";
+    private static Map<String,Integer> bookMarkList  = new HashMap<>();
+
+    /**
+     * 保存进度
+     * @param file 文件
+     * @param index 当前进度
+     * @return 成功与否
+     */
+    public static boolean save(File file, int index){
+        bookMarkList.put(file.getAbsolutePath(),index);
+        try {
+            File saveFile = new File(savePath);
+            if(!saveFile.exists()){
+                saveFile.createNewFile();
+            }
+            ObjectOutputStream oos = new ObjectOutputStream(
+                    new FileOutputStream(saveFile)
+            );
+            oos.writeObject(bookMarkList);
+            oos.close();
+            return true;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    /**
+     * 读取进度
+     * @param file 文件
+     * @return 最近的书签记录，没有则返回1
+     */
+    public static int read(File file){
+        try {
+            ObjectInputStream ois = new ObjectInputStream(
+                    new FileInputStream(savePath)
+            );
+            bookMarkList = (Map<String, Integer>) ois.readObject();;
+            ois.close();
+            return bookMarkList.get(file.getAbsolutePath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return 1;
+    }
+}
